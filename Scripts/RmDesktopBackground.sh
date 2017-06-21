c41d104231111ca50321142dfbdaa8fe1cc17593
Eric Gjerde <ericgjerde@mac.com>
2017-06-21 13:40:21 -0500

    updates

---
+++ Scripts/RmDesktopBackground.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This script changes the Apple Mac OSX wallpaper in 10.10 for the current user
+# to Earth Horizon - feel free to set a different JPEG or download, then set it!
+# Cribbed from this answer: http://stackoverflow.com/a/2119076
+#
+rm "/Library/Desktop Pictures/greg_bday_desktop-01.png"
+#
+
